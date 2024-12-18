98da5d103cf25a6beed18859cf94449d069c193b
fortmarek <marekfort@me.com>
2024-12-18 13:42:59 +0100

    Use static-linux branch of swift-package-manager

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.2.2")),
-        .package(url: "https://github.com/apple/swift-package-manager.git", branch: "main"),
+        .package(url: "https://github.com/tuist/swift-package-manager.git", branch: "static-linux"),
         .package(url: "https://github.com/swift-server/swift-backtrace.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
